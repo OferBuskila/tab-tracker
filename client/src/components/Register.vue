206284332a03c323ad8d575566fad6aff09fb1ea
Ofer <ofer@pointer.co.il>
2019-05-08 01:07:20 +0300

    Add registeration path

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="hello">
+  <div >
     <h1>Resister</h1>
+    <input     type="email"      name="email"      v-model="email"     placeholder="email"      />
+    <input type="password"  v-model="password" name="password" placeholder="password" />
+    <br>
+    <button @click='register' >Register</button>
   </div>
 </template>
 
 <script>
+import AuthenticationService from '@/services/AuthenticationService'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      email: '',
+      password: ''
+
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('emai vhnagre value', value)
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response)
     }
   }
 }
