aadeff17310b3ecb3930617b3626af9574f5d483
Ofer <ofer@pointer.co.il>
2019-05-11 23:43:12 +0300

    login screen  + input validation

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,39 +1,65 @@
 <template>
-  <div >
-    <h1>Resister</h1>
-    <input     type="email"      name="email"      v-model="email"     placeholder="email"      />
-    <input type="password"  v-model="password" name="password" placeholder="password" />
-    <br>
-    <button @click='register' >Register</button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <panel title="Register">
+        <form name="tab-tracker-form"
+          autocomplete="off">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+            autocomplete="new-password"
+          ></v-text-field>
+        </form>
+        <br>
+        <div class="danger-alert" v-html="error" />
+        <br>
+        <v-btn
+          dark
+          class="cyan"
+          @click="register">
+          Register
+        </v-btn>
+      </panel>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
 export default {
-  name: 'HelloWorld',
   data () {
     return {
       email: '',
-      password: ''
-
-    }
-  },
-  watch: {
-    email (value) {
-      console.log('emai vhnagre value', value)
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response)
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+        console.log(response)
+        // this.$store.dispatch('setToken', response.data.token)
+        // this.$store.dispatch('setUser', response.data.user)
+        this.$router.push({
+          // name: 'songs'
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
+
 <style scoped>
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-Vue.config.productionTip = false
-
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
